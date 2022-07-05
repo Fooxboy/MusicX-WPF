cea45d203ccbad6935f331ccafcdddc46b87a5ba
zznty <94796179+zznty@users.noreply.github.com>
2022-07-05 23:37:12 +0700

    fixes #65 (Кнопка в музыке друзей никак не обрабатывается)

---
+++ MusicX/ViewModels/Controls/BlockButtonViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Input;
+using DryIoc;
+using MusicX.Core.Models;
+using MusicX.Core.Services;
+using MusicX.Services;
+using NLog;
+using WPFUI.Common;
+namespace MusicX.ViewModels.Controls;
+
+public class BlockButtonViewModel : BaseViewModel
+{
+    public BlockButtonViewModel(Button action)
+    {
+        Action = action;
+        InvokeCommand = new RelayCommand(Invoke);
+    }
+
+    public Button Action { get; }
+
+    public ICommand InvokeCommand { get; }
+    
+    private async void Invoke()
+    {
+        try
+        {
+            switch (Action.Action.Type)
+            {
+                case "play_shuffled_audios_from_block" or "play_audios_from_block":
+                {
+                    var vkService = StaticService.Container.Resolve<VkService>();
+                    var playerService = StaticService.Container.Resolve<PlayerService>();
+
+                    var res = await vkService.GetBlockItemsAsync(Action.BlockId);
+
+                    await playerService.Play(0, res.Audios);
+                    break;
+                }
+                case "create_playlist":
+                {
+                    var notificationService = StaticService.Container.Resolve<NotificationsService>();
+                    notificationService.Show("Ошибка", "MusicX пока что не умеет создавать плейлисты");
+                    break;
+                }
+                case "open_section":
+                {
+                    var navigation = StaticService.Container.Resolve<NavigationService>();
+
+                    await navigation.OpenSection(Action.SectionId, true);
+                    break;
+                }
+            }
+
+        }
+        catch (Exception e)
+        {
+            StaticService.Container.Resolve<Logger>().Error(e, "Failed to invoke action {Type}", Action.Action.Type);
+        }
+    }
+}
