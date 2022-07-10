7b79870f317373c431bf0a0a0551dfcb9e0eae7a
Славик Лис <fooxboy@yandex.ru>
2022-07-10 13:10:00 +0300

    version 0.29 release

--- MusicX/Services/StaticService.cs
+++ MusicX/Services/StaticService.cs
@@ -6,9 +6,9 @@ namespace MusicX.Services
     public static class StaticService
     {
         public static IContainer Container { get; set; }
-        public static string Version = "0.28.1";
+        public static string Version = "0.29";
         public static string VersionKind = "beta";
-        public static string BuildDate = "1 июня 2022";
+        public static string BuildDate = "10 июля 2022";
 
         public static WebClient WebClient { get; set; }
     }
