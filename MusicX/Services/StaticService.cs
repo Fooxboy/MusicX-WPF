847f311e18a8b7e5c050a0d957c083f6b0468da3
zznty <94796179+zznty@users.noreply.github.com>
2022-08-15 03:07:00 +0700

    bump version to 0.31

--- MusicX/Services/StaticService.cs
+++ MusicX/Services/StaticService.cs
@@ -6,9 +6,9 @@ namespace MusicX.Services
     public static class StaticService
     {
         public static IContainer Container { get; set; }
-        public static string Version = "0.30";
+        public static string Version = "0.31";
         public static string VersionKind = "beta";
-        public static string BuildDate = "4 августа 2022";
+        public static string BuildDate = "14 августа 2022";
 
         public static WebClient WebClient { get; set; }
     }
