bf6d36d4172a0cae30d51157441a82553113c075
Tamerlan <tamikappaev@yandex.ru>
2022-08-17 23:12:29 +0300

    Init

---
+++ MusicX.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MusicX.Avalonia.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
