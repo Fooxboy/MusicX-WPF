39b3133b053fb21ab990f27fc8a72c95d5704bfb
Славик Лис <fooxboy@yandex.ru>
2022-10-20 01:37:34 +0300

    Edit backend

---
+++ MusicX.Server/Models/Callbacks.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicX.Server.Models
+{
+    public class Callbacks
+    {
+        public const string TrackChanged = "TrackChanged";
+        public const string PlayStateChanged = "PlayStateChanged";
+    }
+}
