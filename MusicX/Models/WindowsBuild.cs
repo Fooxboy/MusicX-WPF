44edc18f5ac8b5eed1e7c86721af2b50e5392a5c
Славик Лис <fooxboy@yandex.ru>
2022-12-27 18:12:16 +0300

    #260 added unsopported os info in login view

---
+++ MusicX/Models/WindowsBuild.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicX.Models
+{
+    internal class WindowsBuild
+    {
+        /// <summary>
+        /// Windows XP (Whistler, Freestyle, Harmony)
+        /// </summary>
+        public const int WindowsXP = 2600;
+
+        /// <summary>
+        /// Windows XP (Symphony)
+        /// </summary>
+        public const int WindowsXP_Symphony = 2700;
+
+        /// <summary>
+        /// Windows XP (Emerald)
+        /// </summary>
+        public const int WindowsXP_Emerald = 2710;
+
+        /// <summary>
+        /// Windows XP (Anvil)
+        /// </summary>
+        public const int WindowsXP_Anvil = 3790;
+
+        /// <summary>
+        /// Windows Vista (Longhorn)
+        /// </summary>
+        public const int WindowsVista = 6002;
+
+        /// <summary>
+        /// Windows 7
+        /// </summary>
+        public const int Windows7 = 7601;
+
+        /// <summary>
+        /// Windows 8
+        /// </summary>
+        public const int Windows8 = 9200;
+
+        /// <summary>
+        /// Windows 8.1
+        /// </summary>
+        public const int Windows81 = 9600;
+
+        /// <summary>
+        /// Windows 10 1507 (Threshold)
+        /// </summary>
+        public const int Windows10_1507 = 10240;
+
+        /// <summary>
+        /// Windows 10 1511 (Threshold 2)
+        /// </summary>
+        public const int Windows10_1511 = 10586;
+
+        /// <summary>
+        /// Windows 10 1607 (Redstone 1)
+        /// </summary>
+        public const int Windows10_1607 = 14393;
+
+        /// <summary>
+        /// Windows 10 1704 (Redstone 2)
+        /// </summary>
+        public const int Windows10_1703 = 15063;
+
+        /// <summary>
+        /// Windows 10 1709 (Redstone 3)
+        /// </summary>
+        public const int Windows10_1709 = 16299;
+
+        /// <summary>
+        /// Windows 10 1803 (Redstone 4)
+        /// </summary>
+        public const int Windows10_1803 = 17134;
+
+        /// <summary>
+        /// Windows 10 1809 (Redstone 5)
+        /// </summary>
+        public const int Windows10_1809 = 17763;
+
+        /// <summary>
+        /// Windows 10 1909 (Vanadium)
+        /// </summary>
+        public const int Windows10_1909 = 18363;
+
+        /// <summary>
+        /// Windows 10 2004 (Vanadium)
+        /// </summary>
+        public const int Windows10_2004 = 19041;
+
+        /// <summary>
+        /// Windows 10 20H2 (Vanadium)
+        /// </summary>
+        public const int Windows10_20H2 = 19042;
+
+        /// <summary>
+        /// Windows 10 21H1 (Vanadium)
+        /// </summary>
+        public const int Windows10_21H1 = 19043;
+
+        /// <summary>
+        /// Windows 10 21H2 (Vanadium)
+        /// </summary>
+        public const int Windows10_21H2 = 19044;
+
+        /// <summary>
+        /// Windows 10 22H2 (Vanadium)
+        /// </summary>
+        public const int Windows10_22H2 = 19045;
+
+        /// <summary>
+        /// Windows 11 21H2 (Sun Valley)
+        /// </summary>
+        public const int Windows11_21H2 = 22000;
+
+        /// <summary>
+        /// Windows 11 22H2 (Sun Valley 2)
+        /// </summary>
+        public const int Windows11_22H2 = 22621;
+    }
+}
