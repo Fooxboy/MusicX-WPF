4ea09df6e2e4d6f8cc0083a1f84674dbc9ff34e0
zznty <94796179+zznty@users.noreply.github.com>
2023-04-18 20:52:23 +0700

    more sliders, recomms block list

---
+++ MusicX.Avalonia/Controls/LinkButton.axaml.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace MusicX.Avalonia.Controls;
+
+public class LinkButton : Button
+{
+    public static readonly StyledProperty<string> ImageUrlProperty = AvaloniaProperty.Register<LinkButton, string>(
+        "ImageUrl");
+
+    public string ImageUrl
+    {
+        get => GetValue(ImageUrlProperty);
+        set => SetValue(ImageUrlProperty, value);
+    }
+
+    public static readonly StyledProperty<string> TitleProperty = AvaloniaProperty.Register<LinkButton, string>(
+        "Title");
+
+    public string Title
+    {
+        get => GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public static readonly StyledProperty<string> SubtitleProperty = AvaloniaProperty.Register<LinkButton, string>(
+        "Subtitle");
+
+    public string Subtitle
+    {
+        get => GetValue(SubtitleProperty);
+        set => SetValue(SubtitleProperty, value);
+    }
+}
---
+++ MusicX.Avalonia/Controls/RecommendedPlaylist.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace MusicX.Avalonia.Controls;
+
+public class RecommendedPlaylist : TemplatedControl
+{
+    public static readonly StyledProperty<MusicX.Avalonia.Core.Blocks.RecommendedPlaylist> PlaylistProperty =
+        AvaloniaProperty.Register<RecommendedPlaylist, MusicX.Avalonia.Core.Blocks.RecommendedPlaylist>(
+            "Playlist");
+
+    public MusicX.Avalonia.Core.Blocks.RecommendedPlaylist Playlist
+    {
+        get => GetValue(PlaylistProperty);
+        set => SetValue(PlaylistProperty, value);
+    }
+}
