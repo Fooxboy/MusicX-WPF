66c0978ac73ce7ab46c7f5cb53d4bbf3d17c8025
zznty <94796179+zznty@users.noreply.github.com>
2023-06-03 00:02:13 +0700

    add dedicated block for music_playlists/recomms_slider

---
+++ MusicX/Controls/Blocks/RecommsPlaylistBlock.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using MusicX.Core.Models;
+
+namespace MusicX.Controls.Blocks;
+
+public partial class RecommsPlaylistBlock : UserControl
+{
+    public static readonly DependencyProperty PlaylistsProperty = DependencyProperty.Register(
+        nameof(Playlists), typeof(IEnumerable<Playlist>), typeof(RecommsPlaylistBlock));
+
+    public IEnumerable<Playlist> Playlists
+    {
+        get => (IEnumerable<Playlist>)GetValue(PlaylistsProperty);
+        set => SetValue(PlaylistsProperty, value);
+    }
+    
+    public RecommsPlaylistBlock()
+    {
+        InitializeComponent();
+    }
+}
