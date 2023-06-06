4749930a5195890006076cc131acbabd873ed413
zznty <94796179+zznty@users.noreply.github.com>
2023-06-06 17:13:28 +0700

    config migration from older installations

--- MusicX/Services/ConfigService.cs
+++ MusicX/Services/ConfigService.cs
@@ -1,23 +1,54 @@
-﻿using MusicX.Models;
-using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Win32;
+using MusicX.Models;
+using Newtonsoft.Json;
+using NLog;
 
 namespace MusicX.Services
 {
     public class ConfigService
     {
+        private readonly Logger _logger;
         private readonly string _configPath;
 
-        public ConfigService()
+        public ConfigService(Logger logger, NotificationsService notificationsService)
         {
+            _logger = logger;
             var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MusicX");
-            
-            if (!Directory.Exists(path))
-                Directory.CreateDirectory(path);
-            
             _configPath = Path.Combine(path, Name);
+
+            if (Directory.Exists(path)) return;
+            
+            Directory.CreateDirectory(path);
+            try
+            {
+                MigrateOldConfig(path);
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, "Failed to migrate config");
+                notificationsService.Show("Ошибка миграции!", "Неудалось использовать настройки из прошлой установки приложения.");
+            }
+        }
+
+        private void MigrateOldConfig(string newPath)
+        {
+            var subKey =
+                Registry.LocalMachine.OpenSubKey(
+                    "SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\MusicX");
+
+            var oldPath = subKey?.GetValue("InstallLocation") as string ?? Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "MusicX");
+
+            oldPath = Path.Combine(oldPath, Name);
+            
+            if (!File.Exists(oldPath))
+                return;
+            
+            _logger.Info("Migrating config from {0} to {1}", oldPath, newPath);
+            File.Copy(oldPath, newPath);
         }
 
         private const string Name = "config.json";
