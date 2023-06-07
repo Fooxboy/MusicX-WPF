0773905c4e7a951a87763c0368d426ce3b39fb54
zznty <94796179+zznty@users.noreply.github.com>
2023-06-08 01:35:52 +0700

    Update StaticService.cs

--- MusicX/Services/StaticService.cs
+++ MusicX/Services/StaticService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net;
 using System.Reflection;
 
 namespace MusicX.Services
@@ -10,7 +9,5 @@ namespace MusicX.Services
         public static string Version = typeof(StaticService).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "1.0";
         public static string VersionKind = "";
         public static string BuildDate = "8 июня 2023";
-
-        public static WebClient WebClient { get; set; }
     }
 }
