1fa487c3e45ae6f6d73d2ffeee2adfdf72c9bf45
zznty <94796179+zznty@users.noreply.github.com>
2024-04-08 20:07:57 +0700

    refactor track control and move explicit badge to the right as in vk web (fixes #99)

---
+++ MusicX/Helpers/NullTemplateSelector.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace MusicX.Helpers;
+
+[ContentProperty(nameof(PrimaryTemplate))]
+public class NullTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate? NullTemplate { get; set; }
+    
+    public DataTemplate? PrimaryTemplate { get; set; }
+    
+    public override DataTemplate SelectTemplate(object? item, DependencyObject container)
+    {
+        return (item is null ? NullTemplate : PrimaryTemplate) ?? throw new("NullTemplate and PrimaryTemplate must be set");
+    }
+}
