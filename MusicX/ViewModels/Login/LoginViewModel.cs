f6e15dc871bda2eb44fdb61e57ec006f5d59e3b4
zznty <94796179+zznty@users.noreply.github.com>
2025-12-24 21:32:22 +0700

    fix: exchange token not being set upon login

--- MusicX/ViewModels/Login/LoginViewModel.cs
+++ MusicX/ViewModels/Login/LoginViewModel.cs
@@ -4,12 +4,15 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using AsyncAwaitBestPractices.MVVM;
 using Microsoft.Extensions.DependencyInjection;
+using MusicX.Services;
 using MusicX.ViewModels.Modals;
 using MusicX.Views.Login;
 using MusicX.Views.Modals;
 using VkNet.Abstractions;
+using VkNet.Extensions.Auth.Abstractions;
 using VkNet.Extensions.Auth.Models.Auth;
 using VkNet.Extensions.Auth.Models.Ecosystem;
+using IAuthCategory = VkNet.Extensions.Auth.Abstractions.Categories.IAuthCategory;
 using NavigationService = MusicX.Services.NavigationService;
 using OtpCodePage = MusicX.Views.Login.OtpCodePage;
 
@@ -20,15 +23,22 @@ public class LoginViewModel : BaseViewModel
     private readonly IVkApiAuthAsync _auth;
     private readonly NavigationService _navigationService;
     private readonly IServiceProvider _serviceProvider;
+    private readonly ConfigService _configService;
+    private readonly IAuthCategory _authCategory;
+    private readonly IExchangeTokenStore _exchangeTokenStore;
     public ICommand LoginCommand { get; }
     
     public TaskCompletionSource<bool> LoggedIn { get; } = new();
     
-    public LoginViewModel(IVkApiAuthAsync auth, NavigationService navigationService, IServiceProvider serviceProvider)
+    public LoginViewModel(IVkApiAuthAsync auth, NavigationService navigationService, IServiceProvider serviceProvider,
+        ConfigService configService, IAuthCategory authCategory, IExchangeTokenStore exchangeTokenStore)
     {
         _auth = auth;
         _navigationService = navigationService;
         _serviceProvider = serviceProvider;
+        _configService = configService;
+        _authCategory = authCategory;
+        _exchangeTokenStore = exchangeTokenStore;
         LoginCommand = new AsyncCommand<string>(LoginAsync);
     }
 
@@ -43,6 +53,12 @@ public class LoginViewModel : BaseViewModel
 
             await _auth.AuthorizeAsync(new AndroidApiAuthParams(login, null, CodeRequestedAsync,
                 VerificationMethodRequestedAsync: VerificationMethodRequestedAsync));
+
+            var (exchangeToken, profile) = await _authCategory.GetExchangeToken();
+
+            _configService.Config.UserId = profile.Id;
+            _configService.Config.UserName = $"{profile.FirstName} {profile.LastName}";
+            await _exchangeTokenStore.SetExchangeTokenAsync(exchangeToken);
         }
         catch (Exception e)
         {
